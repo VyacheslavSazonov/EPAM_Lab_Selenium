257ea800141820daad6cbda67aefb608943a4516
VyacheslavSazonov <82762776+VyacheslavSazonov@users.noreply.github.com>
2021-05-30 12:25:48 +0300

    Add files via upload

---
+++ Fabric/Product.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace Lab3.Fabric
+{
+    class Product
+    {
+        public String ProductName { get; set; }
+        public String CategoryID { get; set; }
+        public String SupplierID { get; set; }
+        public String UnitPrice { get; set; }
+        public String QuantityPerUnit { get; set; }
+        public String UnitInStock { get; set; }
+        public String UnitOnOrder { get; set; }
+        public String ReorderLevel { get; set; }
+
+        public Product()
+        {
+            ProductName = "Carp";
+            CategoryID = "Seafood";
+            SupplierID = "Tokyo Traders";
+            UnitPrice = "15";
+            QuantityPerUnit = "2 kg box";
+            UnitInStock = "20";
+            UnitOnOrder = "12";
+            ReorderLevel = "15";
+        }
+    }
+}
---
+++ LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace Lab3
+{
+    class LoginPage : AbstractPage
+    {
+        public LoginPage(IWebDriver driver)
+        {
+            AbstractPage.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "Name")]
+        private IWebElement login;
+        [FindsBy(How = How.Id, Using = "Password")]
+        private IWebElement password;
+        [FindsBy(How = How.XPath, Using = "//input [@type='submit']")]
+        private IWebElement submitInp;
+        [FindsBy(How = How.XPath, Using = "//h2")]
+        public IWebElement resultlogout;
+
+        public HomePage GoToLogin()
+        {
+            login.SendKeys("user");
+            password.SendKeys("user");
+            submitInp.Click();
+            return new HomePage(driver);
+        }
+    } 
+}
